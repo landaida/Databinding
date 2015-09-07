39343847e15cbaf6fc159bd86113351abfd3ecae
landaida <landaida.1@gmail.com>
2015-09-07 14:59:58 -0400

    Databinding test

---
+++ app/src/main/java/com/appsparaguay/testlistcontrol/User.java
@@ -0,0 +1,19 @@
+package com.appsparaguay.testlistcontrol;
+
+/**
+ * Created by Administrador on 9/7/2015.
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+}
