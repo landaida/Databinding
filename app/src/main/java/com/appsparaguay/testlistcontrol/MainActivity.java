0fe6aa2b5ef64790d68dc985f008d42b8bea25ff
landaida <landaida.1@gmail.com>
2015-09-09 15:01:42 -0400

    add toolbar al nuevo data binding que usa el tag <layout> que permite un solo tag data y un solo tag de layouts

--- app/src/main/java/com/appsparaguay/testlistcontrol/MainActivity.java
+++ app/src/main/java/com/appsparaguay/testlistcontrol/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -28,7 +29,7 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
         });
         this.getPublicTimeline();
 
-
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
     }
 
     public void getPublicTimeline() {
